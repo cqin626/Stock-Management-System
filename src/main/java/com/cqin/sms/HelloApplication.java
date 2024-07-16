f01e867727b2bb26444e6d71229b6012fedcc29d
cqin626 <qinhoh@gmail.com>
2024-07-17 03:12:31 +0800

    develop main menu

---
+++ src/main/java/com/cqin/sms/HelloApplication.java
@@ -0,0 +1,40 @@
+package com.cqin.sms;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    double x, y = 0;
+    @Override
+    public void start(Stage pStage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        pStage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(e -> {
+           x = e.getSceneX();
+           y = e.getSceneY();
+        });
+
+        root.setOnMouseDragged(e -> {
+            pStage.setX(e.getScreenX() - x);
+            pStage.setY(e.getScreenY() - y);
+        });
+
+        pStage.setScene(new Scene(root));
+        pStage.setTitle("Undecorated Stage Example");
+        pStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
